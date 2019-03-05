ba8c7b4ad903c6cbd6badc4031a56ddaebc35928
Sebastian Hanu <shanu@totalsoft.ro>
2019-03-05 13:51:28 +0200

    use re-reselect

--- src/selectors/myPosts.js
+++ src/selectors/myPosts.js
@@ -1,19 +1,20 @@
 import { createSelector } from 'reselect'
+import createCachedSelector from 're-reselect'
 import * as postActions from 'actions/posts';
 
-export const getPosts = (filter, orderBy, pageSize, state) => {
+const ids = (filter, orderBy, pageSize, state) => {
     const key = postActions.loadPostsKey(filter, orderBy, pageSize);
+    return (state.myPosts[key] && state.myPosts[key].ids ? state.myPosts[key].ids : []);    
+}
+const posts = (filter, orderBy, pageSize, state) => (state.entities.posts ? state.entities.posts : [])
 
-    const ids = state => (state.myPosts[key] && state.myPosts[key].ids ? state.myPosts[key].ids : [])
-    const posts = state => state.entities.posts
-
-    const selector = createSelector(
-        [ids, posts],
-        (ids, posts) => ids.map(id => posts[id])
-    )
-
-    return selector(state);
-};
+export const getPosts = createCachedSelector(
+    ids,
+    posts,
+    (ids, posts) => ids.map(id => posts[id])
+)(
+    (filter, orderBy, pageSize, state) => postActions.loadPostsKey(filter, orderBy, pageSize)
+);
 
 export const getPostsLoading = (filter, orderBy, pageSize, state) => {
     const key = postActions.loadPostsKey(filter, orderBy, pageSize);
