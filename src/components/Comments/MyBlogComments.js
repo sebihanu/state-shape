b094aa918f33a3cf0f4e3de5bfe08648be8b6fab
Sebastian Hanu <shanu@totalsoft.ro>
2019-03-13 10:37:23 +0200

    myblogcomments

--- src/components/Comments/MyBlogComments.js
+++ src/components/Comments/MyBlogComments.js
@@ -1,37 +1,25 @@
-import React, { PureComponent } from "react";
+import React from "react";
 import PropTypes from 'prop-types';
 import { Button } from '@material-ui/core';
 import Comment from './Comment';
 
-export default class MyBlogComments extends PureComponent {
-    componentDidMount() {
-        const { blogId, page, pageSize } = this.props;
-        this.props.actions.loadBlogComments(blogId, page, pageSize);
-    }
-
-    componentDidUpdate(prevProps) {
-        const { blogId, page, pageSize } = this.props;
-        if (prevProps.page !== page) {            
-            this.props.actions.loadBlogComments(blogId, page, pageSize);
-        }
-    }
-
-    render() {
-        const { comments, commentsLoading, loadMore } = { ...this.props };
-        return (
-            <div>
-                <Button onClick={loadMore} disabled={commentsLoading}>Load more...</Button>
-                {commentsLoading && (<div>Loading</div>)}
-                {comments && comments.map(c => (
-                    <Comment key={c.id} {...c} />
-                ))}
-            </div>
-        );
-    }
+const MyBlogComments = (props) => {
+    const { comments, commentsLoading, loadMore } = { ...props };
+    return (
+        <div>
+            <Button onClick={loadMore} disabled={commentsLoading}>Load more...</Button>
+            {commentsLoading && (<div>Loading</div>)}
+            {comments && comments.map(c => (
+                <Comment key={c.id} {...c} />
+            ))}
+        </div>
+    );
 }
 
-MyBlogComments.propTypes = {
-    pageSize: PropTypes.number.isRequired,
+MyBlogComments.propTypes = {    
     comments: PropTypes.array,
-    commentsLoading: PropTypes.bool.isRequired
+    commentsLoading: PropTypes.bool.isRequired,
+    loadMore: PropTypes.func.isRequired,
 };
+
+export default MyBlogComments;
--- src/containers/Dashboard/MyBlogCommentsWidget.js
+++ src/containers/Dashboard/MyBlogCommentsWidget.js
@@ -1,23 +1,48 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
 import { compose } from 'redux'
 import { connect } from 'react-redux';
 import { loadBlogComments } from 'actions/comments';
 import { getComments, getCommentsPageLoading } from 'selectors/blogComments'
 import MyBlogComments from 'components/Comments/MyBlogComments'
-import React, { Component } from 'react';
 
-class MyBlogCommentsWidget extends Component {
-    state = { page: 1 }
+class MyBlogCommentsWidget extends PureComponent {
+    componentDidMount() {
+        const { blogId, page, pageSize } = this.props;
+        this.props.actions.loadBlogComments(blogId, page, pageSize);
+    }
+
+    componentDidUpdate(prevProps) {
+        const { blogId, page, pageSize } = this.props;
+        if (prevProps.page !== page) {            
+            this.props.actions.loadBlogComments(blogId, page, pageSize);
+        }
+    }
 
     loadMore = () => {
         this.setState((prevState) => ({ page: prevState.page + 1 }));
     }
 
     render = () => {
+        const { comments, commentsLoading, loadMore } = this.props;
+        const myBlogCommentsProps = { comments, commentsLoading, loadMore };
         return (
-            <MyBlogCommentsConnected {...this.state} {...this.props} loadMore={this.loadMore} />
+            <MyBlogComments {...myBlogCommentsProps} />
         );
     }
 }
+
+MyBlogCommentsWidget.propTypes = {    
+    comments: PropTypes.array.isRequired,
+    commentsLoading: PropTypes.bool.isRequired,
+    loadMore: PropTypes.func.isRequired,
+    page: PropTypes.number.isRequired,
+    pageSize: PropTypes.number.isRequired,
+    blogId: PropTypes.number.isRequired,
+    actions: PropTypes.shape({
+        loadBlogComments: PropTypes.func.isRequired
+    }).isRequired
+};
 
 function mapStateToProps(state, ownProps) {
     const blogId = state.currentUser.blogId;
@@ -39,5 +64,25 @@ function mapDispatchToProps(dispatch) {
     };
 }
 
-const MyBlogCommentsConnected = compose(connect(mapStateToProps, mapDispatchToProps))(MyBlogComments);
-export default MyBlogCommentsWidget;
+const MyBlogCommentsConnected = compose(connect(mapStateToProps, mapDispatchToProps))(MyBlogCommentsWidget);
+
+class MyBlogCommentsLocalState extends PureComponent {
+    state = { page: 1 }
+
+    loadMore = () => {
+        this.setState((prevState) => ({ page: prevState.page + 1 }));
+    }
+
+    render() {
+        return (
+            <MyBlogCommentsConnected {...this.props} {...this.state}
+                loadMore={this.loadMore} />
+        );
+    }
+}
+
+MyBlogCommentsLocalState.propTypes = {        
+    pageSize: PropTypes.number.isRequired
+};
+
+export default MyBlogCommentsLocalState;
