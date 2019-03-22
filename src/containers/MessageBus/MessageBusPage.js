5c3de9d77854876bc97c8c80c8e093690cde71fd
Sebastian Hanu <shanu@totalsoft.ro>
2019-03-22 11:16:22 +0200

    multiple subscribers, load order

--- src/containers/MessageBus/MessageBusPage.js
+++ src/containers/MessageBus/MessageBusPage.js
@@ -6,15 +6,42 @@ import PubSub from 'pubsub-js'
 
 const TOPIC1 = "TOPIC1";
 class MessageBusPage extends PureComponent {
+    state = {
+        showPublisher: false,
+        showSubscriber1: false,
+        showSubscriber2: false
+    }
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({ showPublisher: true });
+        }, 4000);
+        setTimeout(() => {
+            this.setState({ showSubscriber1: true });
+        }, 0);
+        setTimeout(() => {
+            this.setState({ showSubscriber2: true });
+        }, 8000);
+    }
+
     render() {
         return (
             <Grid container spacing={24}>
-                <Grid item sm={4}>
-                    <Publisher PubSub={PubSub} topic={TOPIC1} />
-                </Grid>
-                <Grid item sm={4}>
-                    <Subscriber PubSub={PubSub} topic={TOPIC1}/>
-                </Grid>
+                {this.state.showPublisher &&
+                    (<Grid item sm={4}>
+                        <Publisher PubSub={PubSub} topic={TOPIC1} />
+                    </Grid>)
+                }
+                {this.state.showSubscriber1 &&
+                    (<Grid item sm={4}>
+                        <Subscriber PubSub={PubSub} topic={TOPIC1} />
+                    </Grid>)
+                }
+                {this.state.showSubscriber2 &&
+                    (<Grid item sm={4}>
+                        <Subscriber PubSub={PubSub} topic={TOPIC1} />
+                    </Grid>)
+                }
             </Grid>
         );
     }
